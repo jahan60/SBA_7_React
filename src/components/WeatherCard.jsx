0d84b7d1bfb2256c23cc902b3f97b05df53c16d7
jahan60 <faridajahan.ny@gmail.com>
2026-02-27 12:54:05 -0500

    completed display logic

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,9 +1,15 @@
-function WeatherCard() {
+function WeatherCard({ loading, error, weather }) {
+  if(loading) return <p>Loading...</p>;
+  if(error) return <p className="error">{}error</p>
+  if(!weather) return null;
+
   return (
     <div className="weather-card">
-      <p>WeatherCard Component</p>
+      <h2>{weather.name}</h2>
+      <p>{weather.main.temp}°C</p>
+      <p>{weather.weather[0].description}</p>
     </div>
-  )
+  );
 }
 
 export default WeatherCard
